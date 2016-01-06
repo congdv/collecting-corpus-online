c5f6a6a304951be96a8f2ee4e7af1d313e9d2596
hoaibang07 <hoaibang07@gmail.com>
2016-01-06 12:58:58 +0700

    remove file

--- Vietgle/main/main_vietgle.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-html = urlopen("http://tratu.coviet.vn/hoc-tieng-trung/cap-cau-song-ngu/vietgle-tra-tu/tat-ca/trang-1.html")
-bsObj = BeautifulSoup(html)
-for child in bsObj.findAll("ul",{"class":"uccss"}):
-    print (child.find("li",{"class":"ctk"}).get_text())
-    print (child.find("li",{"class":"p10l"}).get_text())
