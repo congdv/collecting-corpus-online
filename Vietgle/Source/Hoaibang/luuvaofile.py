408b0b7f232f20d9dd9ea2423a854705361ca7c3
hoaibang07 <hoaibang07@gmail.com>
2016-01-06 10:19:20 +0700

    Commit All change

---
+++ Vietgle/Source/Hoaibang/luuvaofile.py
@@ -0,0 +1,20 @@
+#import
+import os.path
+from urllib import urlopen
+from bs4 import BeautifulSoup
+import io
+
+filename = '/media/hbc/DLIEU/Output.txt'
+f = io.open(filename, 'w', encoding='utf-16')
+
+for num in range(1,3):
+	numStr = str(num)
+	url = "http://tratu.coviet.vn/hoc-tieng-trung/cap-cau-song-ngu/vietgle-tra-tu/tat-ca/trang-" + numStr + ".html"
+	html = urlopen(url)
+	bsObj = BeautifulSoup(html)
+	for child in bsObj.findAll("ul",{"class":"uccss"}):
+	    s1 = child.find("li",{"class":"ctk"}).get_text()
+	    s2 = child.find("li",{"class":"p10l"}).get_text()
+	    f.write(s1 + '\n')
+	    f.write(s2 + '\n')
+f.close()
