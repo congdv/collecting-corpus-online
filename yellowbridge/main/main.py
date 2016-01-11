3e610e9d53c597326120da52682ec761a951d305
congdv <congdaovan94@gmail.com>
2016-01-11 10:57:31 +0700

    Add cookie browser and open web browser for type captcha

--- yellowbridge/main/main.py
+++ yellowbridge/main/main.py
@@ -3,6 +3,8 @@ import time
 import urllib2
 from bs4 import BeautifulSoup
 import io
+import webbrowser
+import requests
 
 def main(wordList):
     i = 1
@@ -10,10 +12,21 @@ def main(wordList):
     #start infor mation about a word
         f.write(u"-----Start word------\n")
         #content process
-        time.sleep(5)
-        the_page = getResponseFromPostMethod('E',word)
-        print the_page.read()
+        #get content that request to website
+        try:
+            the_page = getResponseFromPostMethod('E',word)
+        #handle error request forbidden
+        except urllib2.HTTPError as error:
+            print error
+            return None
+        #use lib for parse html struct
         soup = BeautifulSoup(the_page)
+        #get title page
+        if soup.find('title').get_text()=='YellowBridge Human User Verification':
+            webbrowser.open_new_tab('http://www.yellowbridge.com/general/captcha.php')
+            time.sleep(10)
+            the_page = getResponseFromPostMethod('E',word)
+            soup = BeautifulSoup(the_page)
 		
         print word
         #find all tr tag, get text with '|' separator
@@ -23,8 +36,10 @@ def main(wordList):
 	        f.write(trtag.get_text('|') + '\n')
                 print 'In tu'+trtag.get_text()
         except AttributeError as error:
-            f.write(word.decode('utf-8') + '\n')
-            print error
+            print "not found html tag because forbidden"
+            #url = 'http://www.yellowbridge.com/chinese/dictionary.php' 
+
+            #webbrowser.open_new_tab(url)
         #end infomation about a word
         f.write(u"-----End word-------\n")
         print i
@@ -45,15 +60,16 @@ def getResponseFromPostMethod(searchMode, word):
     values = {'searchMode' : searchMode,
               'word' : word
              }
-
+    cookies = '_ga=GA1.2.133163078.1451881386; PHPSESSID=rfaq4drk716uj9cgbppp7aijl7; _gat=1'
     #header of request
-    headers = { 'User-Agent' : user_agent }
+    headers = { 'User-Agent' : user_agent,'Cookie':cookies}
     #encode url(values)
     data = urllib.urlencode(values)
     #create request
     req = urllib2.Request(url, data, headers)
     #get response
     response = urllib2.urlopen(req)
+        
     return response
 
 
